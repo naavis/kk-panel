cf81d83ef3635d4fa1473af55eb179dafd542f56
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2017-01-04 00:11:35 +0200

    Add unit tests for ImageManager.

---
+++ test/imageManager.test.js
@@ -0,0 +1,45 @@
+
+var sinon = require('sinon');
+var mock = require('mock-require');
+var assert = require('assert');
+var ImageManager = require('../imageManager.js');
+
+const DUMMY_NAME = 'dummyname';
+const DUMMY_PATH = 'dummypath/';
+const DUMMY_DEST = 'dummypath\\dummyname-latest.jpg';
+const DUMMY_URL = 'http://foo.bar/baz.jpg';
+const DUMMY_URL_NO_EXT = 'http://foo.bar/baz';
+
+describe('imageManager', () => {
+  describe('saveImage', () => {
+    var manager = {};
+    var downloaderSpy = {};
+
+    beforeEach(() => {
+      downloaderSpy = sinon.spy();
+      mock('image-downloader', downloaderSpy);
+      ImageManager = mock.reRequire('../imageManager.js');
+
+      manager = new ImageManager(DUMMY_PATH);
+    });
+
+    afterEach(() => {
+      mock.stopAll();
+    });
+
+    it('calls given URL', () => {
+      manager.saveImage(DUMMY_NAME, DUMMY_URL);
+      assert(downloaderSpy.calledWithMatch({url: DUMMY_URL}));
+    });
+
+    it('uses correct destination', () => {
+      manager.saveImage(DUMMY_NAME, DUMMY_URL);
+      assert(downloaderSpy.calledWithMatch({dest: DUMMY_DEST}));
+    });
+
+    it('adds extension when needed', () => {
+      manager.saveImage(DUMMY_NAME, DUMMY_URL_NO_EXT);
+      assert(downloaderSpy.calledWithMatch({dest: DUMMY_DEST}));
+    });
+  });
+});
