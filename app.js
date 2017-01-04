893e7188956d61cb5db80b006cfb3e6e811d0720
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2017-01-04 22:49:24 +0200

    Add boilerplate server code.

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
+var path = require('path');
+var express = require('express');
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 var Scheduler = require('./scheduler.js');
 var ImageManager = require('./imageManager.js');
 
 var jobs = require('./jobs');
+
+const PORT = 3000;
 
 // Configure job schedules
 var scheduler = new Scheduler();
@@ -14,3 +21,13 @@ scheduler.add('sat24ir', '*/5 * * * *', jobs.sat24ir, imageManager);
 scheduler.add('sat24vis', '*/5 * * * *', jobs.sat24vis, imageManager);
 scheduler.add('kumpula', '* * * * *', jobs.kumpula, imageManager);
 scheduler.startAll();
+
+app.use(express.static(path.join(__dirname, 'images')));
+
+app.get('/', function(req, res) {
+  res.send('Hello world!');
+});
+
+server.listen(PORT, function() {
+  console.log(`Listening on port ${PORT}!`);
+});
