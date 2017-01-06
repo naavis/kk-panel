b38d908da4c3a7b7d42487d8ea04f55e448dbbde
Samuli Vuorinen <samuli.vuorinen@iki.fi>
2017-01-06 10:26:43 +0200

    Save logs to file when in production.

--- src/logger.js
+++ src/logger.js
@@ -1,10 +1,16 @@
 var winston = require('winston');
-
 winston.remove(winston.transports.Console);
 
-const timestampFormat = () => { return (new Date()).toISOString(); };
-winston.add(winston.transports.Console, {
+let timestampFormat = () => ((new Date()).toISOString());
+let options = {
   timestamp: timestampFormat,
-});
+}
+
+if (process.env.NODE_ENV === 'production') {
+  options.filename = 'app.log';
+  winston.add(winston.transports.File, options);
+} else {
+  winston.add(winston.transports.Console, options);
+}
 
 module.exports = winston;
